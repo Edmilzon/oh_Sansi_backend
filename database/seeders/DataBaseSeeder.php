6a8460e1c3eef42437f33958f004a084e47ce070
Maria Belen Quisbert Fernandez <maria.belen.quis@gmail.com>
2025-12-17 23:37:24 -0400

    eliminacion de semilla

--- database/seeders/DataBaseSeeder.php
+++ database/seeders/DataBaseSeeder.php
@@ -21,9 +21,5 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsuariosSeeder::class,
         ]);
-
-        $this->call([
-            ConfiguracionAccionSeeder::class,
-        ]);
     }
 }
